eeccdba349706fd86119f2906d2cad03c6524624
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-04-15 15:41:14 +0530

    Add App file

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import SignUp from "./Component/SignUp";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <SignUp />;
+  }
+}
+
+export default App;
